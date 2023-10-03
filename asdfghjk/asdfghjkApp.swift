062611e67e54d8340adeb026657b32278fcec1fe
Lajsek <karol.lis@pollub.edu.pl>
2023-10-03 12:56:48 +0200

    Initial Commit

---
+++ asdfghjk/asdfghjkApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdfghjkApp.swift
+//  asdfghjk
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct asdfghjkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
